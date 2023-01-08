033fc7b93670bff5741d4d5668ee2a2714fd37c0
ceciaguicam <75157954+ceciaguicam@users.noreply.github.com>
2023-01-08 19:44:17 +0100

    Commit 5: se puede buscar un objeto sin escribir su nombre completo, solo con el primer caracter

--- nodepop/routes/index.js
+++ nodepop/routes/index.js
@@ -4,7 +4,7 @@ const connection = require('../conn/connection.js')
 const mongoosePaginate = require('mongoose-paginate-v2')
 const AdvertisementModel = require('../model/add.js')
 
-const params = ['name', 'sellOrBuy', 'priceBottom', 'priceTop', 'tags']
+const params = ['sellOrBuy', 'priceBottom', 'priceTop', 'name']
 
 
 /* GET home page. */
@@ -14,44 +14,50 @@ let limit = 5
 
 router.get('/api/v1/advertisements/', function(req, res, next) {
 
+  //let tags = req.query.tags
+
   if(req.query.page) {
     page = req.query.page
   }
-  //let page = req.query.page
+  
   if(req.query.limit) {
     limit = req.query.limit
   }
-  //let limit = req.query.limit
-  let name = req.query.name
-  let sellOrBuy = req.query.sellOrBuy
+
+  
+  //let name = req.query.name
+  //let sellOrBuy = req.query.sellOrBuy
   let priceBottom = parseFloat(req.query.priceBottom)
   let priceTop = parseFloat(req.query.priceTop)
   
   const filterValues = {}
 
+  if(req.query.tags) {
+    filterValues.tags = req.query.tags
+  }
+
+  if(req.query.priceBottom || req.query.priceTop){
+    filterValues.price = {}
+    if (req.query.priceBottom){
+      filterValues.price['$gte'] = priceBottom
+    }
+    if(req.query.priceTop){
+      filterValues.price['$lte'] = priceTop
+    }
+  }
+
   params.forEach(param => {
     
     if(req.query.hasOwnProperty(param)){
-      if(req.query.priceBottom || req.query.priceTop){
-        filterValues.price = {}
-        if (req.query.priceBottom){
-          filterValues.price['$gte'] = priceBottom
-        }
-        if(req.query.priceTop){
-          filterValues.price['$lte'] = priceTop
-      }
-      else {
-        filterValues[param] = req.query[param]
-      }
-    }}
+      filterValues[param] = req.query[param]
+    }
   })
 
   console.log(filterValues)
 
-  //let advertisementsValues = {nombre: name, venta: sellOrBuy, precio: {'$gte': priceBottom, '$lte': priceTop}}
+  filterValues.name = new RegExp('^' + req.query.name, 'i')
 
-  //let advertisementsValues = {name: name}
-
+  console.log(filterValues)
 
   const paginationOptions = {
     page: page,
